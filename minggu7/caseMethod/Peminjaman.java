fe4087d5f7c7b30d2c48cdecf5fe9820ae9bf24e
athia26 <widas22.athia@gmail.com>
2026-04-17 08:03:39 +0700

    UTS Sesi 4

--- minggu7/caseMethod/Peminjaman.java
+++ minggu7/caseMethod/Peminjaman.java
@@ -4,22 +4,29 @@ public class Peminjaman {
     Mahasiswa mhs; 
     Buku buku;
     int lamaPinjam, batasPinjam = 5, terlambat, denda;
+    boolean isCanceled = false;
 
-    Peminjaman(Mahasiswa mhs, Buku buku, int lamaPinjam){
+
+    Peminjaman(Mahasiswa mhs, Buku buku, int lamaPinjam, boolean isCanceled){
         this.mhs = mhs;
         this.buku = buku;
         this.lamaPinjam = lamaPinjam;
+        this.isCanceled = false;
         
         hitungDenda();
     }
 
+    //pengerjaan UTS sesi 4
     void hitungDenda(){
         terlambat = lamaPinjam - batasPinjam;
-        if (terlambat<0){
+        if (terlambat<0){ //jika terlambat 0, maka 0 terlambat 
             terlambat = 0;
+        } else if (terlambat > 10){ // jika terlambat lebih dari 10, maka denda muncul menjadi 200000
+            denda=20000;
+        } else{ //jika terlambat kurang dari 10 dan lebih dari 0, maka dihitung seperti biasa.
+            denda = terlambat * 2000;
         }
 
-        denda = terlambat * 2000;
     }
 
     void tampilPeminjaman(){
@@ -28,6 +35,25 @@ public class Peminjaman {
         System.out.print("Lama :  "+lamaPinjam+" | ");
         System.out.print("Terlambat :  "+terlambat+" | ");
         System.out.print("Denda :  "+denda);
+        
         System.out.println();
     }
+
+    //pengerjaan UTS sesi 4
+    void tampilPinjamCanceled(){
+        if (isCanceled == false){ //untuk validasi apakah isCanceled true atau false, jika true, maka tidak akan muncul, jika false akan muncul dengan format dibawha
+            
+            System.out.print(mhs.nama+" | ");
+            System.out.print(buku.judul+" | ");
+            System.out.print("Lama :  "+lamaPinjam+" | ");
+            System.out.print("Terlambat :  "+terlambat+" | ");
+            System.out.print("Denda :  "+denda);
+            //System.out.print(isCanceled); untuk melakukan pengecekan apakah yg muncul sudah false semua 
+
+            System.out.println();
+        }
+        
+    }
+
+
 }
--- minggu7/caseMethod/mainRuangBaca.java
+++ minggu7/caseMethod/mainRuangBaca.java
@@ -22,11 +22,11 @@ public class mainRuangBaca {
         };
 
         Peminjaman[] dataPinjam = {
-            new Peminjaman(dataMhs[0], dataBuku[0], 7),
-            new Peminjaman(dataMhs[1], dataBuku[1], 3),
-            new Peminjaman(dataMhs[2], dataBuku[2], 10),
-            new Peminjaman(dataMhs[2], dataBuku[3], 6),
-            new Peminjaman(dataMhs[0], dataBuku[1], 4)
+            new Peminjaman(dataMhs[0], dataBuku[0], 16, false), //diganti menjadi lama pinjam 16. 16-5 = 11. lebih dari 10. 
+            new Peminjaman(dataMhs[1], dataBuku[1], 3, false),
+            new Peminjaman(dataMhs[2], dataBuku[2], 10, false),
+            new Peminjaman(dataMhs[2], dataBuku[3], 6, false),
+            new Peminjaman(dataMhs[0], dataBuku[1], 4, false)
 
         };
 
@@ -37,7 +37,7 @@ public class mainRuangBaca {
             System.out.println("2. Tampilkan Buku");
             System.out.println("3. Tampilkan Peminjaman");
             System.out.println("4. Urutkan Berdasarkan Denda");
-            System.out.println("5. Cari Berdasarkan NIM");
+            System.out.println("5. Cari Berdasarkan NIM untuk cancel");
             System.out.println("6. Keluar");
             System.out.println("====================================");
             System.out.print("Pilih menu (1-6) : ");
@@ -57,41 +57,55 @@ public class mainRuangBaca {
             } else if (pilihan == 3){
                 System.out.println("\n--- DATA PEMINJAMAN ---");
                 for (int i = 0; i < dataPinjam.length; i++) {
-                    dataPinjam[i].tampilPeminjaman();
+                    dataPinjam[i].tampilPinjamCanceled(); //untuk menampilkan data peminjam setelah cancel, data yg cancel akan hilang
                 }
             } else if (pilihan == 4){
                 //insertion-sort
-                for (int i = 1; i < dataPinjam.length; i++) {
-                    Peminjaman temp = dataPinjam[i]; //untuk simpan 
-                    int j = i-1;
-
+                for (int i = 1; i < dataPinjam.length; i++) { 
+                    Peminjaman temp = dataPinjam[i]; 
+                    
+                    int j = i-1; 
                     while (j >= 0 && dataPinjam[j].denda < temp.denda){
-                        ///kalau data masih di dalam array, dan denda di kiri array lebih kecil dari temp, maka yg lebih kecil 
-                        
-                        dataPinjam [j+1] = dataPinjam[j]; //misal lagi di j = 0, berarti data [1], ditukar dengan data 
-                        j = j-1;
+                       
+                        dataPinjam [j+1] = dataPinjam[j]; 
+                        j--; 
                     }
                     dataPinjam[j+1] = temp;
                 }
                 System.out.println("Setelah diurutkan (Denda terbesar)");
                 for (int i = 0; i < dataPinjam.length; i++) {
-                    dataPinjam[i].tampilPeminjaman();
+                    dataPinjam[i].tampilPinjamCanceled(); //untuk menampilkan data peminjam setelah cancel, data yg terurut adalah data yang tidak cancel 
                 }
 
             } else if (pilihan == 5){
-
-                System.out.print("Masukkan NIM yang dicari : ");
+                System.out.print("Masukkan NIM yang akan dicancel : ");
                 String cari = athia.nextLine();
-
-                System.out.println("Hasil pencarian NIM: ");
+                
                 boolean ketemu = false; 
+
+                //pengerjaan UTS sesi 4
 
                 for (int i = 0; i < dataPinjam.length; i++) {
                     if(dataPinjam[i].mhs.nim.equals(cari)){
-                        dataPinjam[i].tampilPeminjaman();
+                        System.out.println("\nData yang ditemukan: ");
+                        dataPinjam[i].tampilPeminjaman(); //menampilkan data yang dicari 
                         ketemu = true;
+
+                        System.out.print("\nApakah akan mencancel peminjaman tersebut (y/n) ?  "); //konfirmasi akan menghapus atau tidak 
+                        String ya = athia.nextLine();
+                        
+                        if (ya.equalsIgnoreCase("Y")){ //jika pengguna mengetik ya, maka is canceled akan menjadi true, data pengguna akan "terhapus"
+                            dataPinjam [i].isCanceled = true;
+                        } 
+                        
                     }
                 }
+
+                System.out.println("\nData Peminjam yang tidak cancel: "); //untuk menampilkan data yang tidak di cancel, dengan is canceled adalah false. 
+                for (int i = 0; i < dataPinjam.length; i++) {
+                    dataPinjam[i].tampilPinjamCanceled();
+                }
+                
 
                 if (!ketemu){
                     System.out.println("Data tidak ditemukan");
